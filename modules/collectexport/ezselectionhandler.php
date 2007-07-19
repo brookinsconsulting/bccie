14a6c324816a121726e20de85878ddda96651533
Brookins Consulting <info@brookinsconsulting.com>
2007-07-19 21:30:45 +0000

    Updated: Added ezselection name output to handler

--- modules/collectexport/ezselectionhandler.php
+++ modules/collectexport/ezselectionhandler.php
@@ -3,8 +3,28 @@ include_once('extension/collectexport/modules/collectexport/parser.php');
 
 class eZSelectionHandler extends BaseHandler{
        function exportAttribute(&$attribute, $seperationChar) {
-                $content=&$attribute->content();
-                return $this->escape($content[0], $seperationChar);
+	        $ret = false;
+                
+		$content=&$attribute->content();
+		$selectionID=$content[0];
+
+		// $selectionAttribute=&$attribute["ContentObjectAttributeID"];
+		$contentObjectAttribute = $attribute->contentObjectAttribute();
+		$contentClassAttribute = $attribute->contentClassAttribute();
+		$contentClassAttributeContent = $contentClassAttribute->content();
+		if( isset( $contentClassAttributeContent['options'] ) )
+		{
+		    $options = $contentClassAttributeContent['options'];
+		    $selectionOptionName = $options[$selectionID]['name'];
+
+		    // print("\n\nBegin Options \n\n");
+		    // print("Selection: $selectionID\n");
+		    // print("Selection Name: $selectionOptionName\n");
+		    $ret = $selectionOptionName;
+		}
+		// print_r("$ret\n");
+	       return $this->escape($ret, $seperationChar);
+               // return $this->escape($content[0], $seperationChar);
        }
 }
 
