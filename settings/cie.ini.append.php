164c1fb33effba64f9621d266e44e1d79538bb45
Brookins Consulting <info@brookinsconsulting.com>
2007-07-19 21:29:54 +0000

    Updated: Relocated extension settings into separate settings file, cie.ini

---
+++ settings/cie.ini.append.php
@@ -0,0 +1,74 @@
+<?php /* #?ini charset="utf8"?
+
+[ExportCollectionCSVSettings]
+# Debug=enabled
+Debug=disabled
+
+# Export Log File
+Log=var/log/exportcsv.log
+LogDebug=disabled
+
+# Export Directory
+Directory=var/export
+
+# Export Format
+Format=csv
+# Format=sylk
+
+# Export Separator
+Separator=;
+# Separator=|
+# Separator=,
+# Separator=:
+# Separator=#
+
+# Collections to export to file
+Collection[]
+#Collection[]=206
+#Collection[]=207
+#Collection[]=3248
+#Collection[]=2055
+#Collection[]=3250
+
+ExcludeAttributeID[]
+# ExcludeAttributeID[]=001
+# ExcludeAttributeID[]=002
+# ExcludeAttributeID[]=003
+
+
+[ExportCollectionSylkSettings]
+# Debug=enabled
+Debug=disabled
+
+# Export Log File
+Log=var/log/exportsylk.log
+LogDebug=disabled
+
+# Export Directory
+Directory=var/export
+
+# Export Format
+Format=sylk
+# Format=csv
+
+# Export Separator
+Separator=;
+# Separator=|
+# Separator=,
+# Separator=:
+# Separator=#
+
+# Collections to export to file
+Collection[]
+#Collection[]=206
+#Collection[]=207
+#Collection[]=3248
+#Collection[]=2055
+#Collection[]=3250
+
+ExcludeAttributeID[]
+# ExcludeAttributeID[]=001
+# ExcludeAttributeID[]=002
+# ExcludeAttributeID[]=003
+
+*/ ?>
--- settings/cronjob.ini.append.php
+++ settings/cronjob.ini.append.php
@@ -1,5 +1,4 @@
-<?
-/*
+<?php /* #?ini charset="utf8"?
 
 [CronjobSettings]
 ExtensionDirectories[]=collectexport
@@ -8,33 +7,8 @@ ExtensionDirectories[]=collectexport
 Scripts[]
 Scripts[]=exportcsv.php
 
-[ExportCollectionCSVSettings]
-# Debug=enabled
-Debug=disabled
-# Export Log File
-Log=var/log/exportcsv.log
-LogDebug=disabled
-# Export Directory
-Directory=var/export
-# Export Format
-Format=csv
-# Format=sylk
-# export Separator
-Separator=|
-# Separator=,
-# Separator=;
-# Separator=:
-# Separator=#
-#
-# Collections to export to file
-Collection[]
-Collection[]=3248
-Collection[]=2055
-Collection[]=3250
-ExcludeAttributeID[]
-# ExcludeAttributeID[]=001
-# ExcludeAttributeID[]=002
-# ExcludeAttributeID[]=003
+[CronjobPart-exportslyk]
+Scripts[]
+Scripts[]=exportslyk.php
 
-*/
-?>
+*/ ?>
