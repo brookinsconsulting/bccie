9cef32efe67369ac56bade058c4b1b86b4fcd453
Brookins Consulting <info@brookinsconsulting.com>
2013-10-22 18:07:16 -0500

    Added: Added bin/runcronjobpartsylk.sh script

---
+++ bin/runcronjobpartsylk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# php ./runcronjobs.php -dall exportcsv;
+# php ./runcronjobs.php -dall exportcsv | tee var/log/cie.log
+# php ./runcronjobs.php -dall exportsylk | tee var/log/cie.log
+
+php ./runcronjobs.php -dall exportsylk | tee var/log/cie.log
