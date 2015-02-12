7ed9869136b27884f86b908ed5d68f9b5ee10e79
Vincent Guyard <vguyard@onaxis.cl>
2015-02-12 12:05:17 -0300

    Fix the missing of the include of basehandler.php

--- classes/handlers/smileobjectrelationlisthandler.php
+++ classes/handlers/smileobjectrelationlisthandler.php
@@ -2,7 +2,14 @@
 /**
  * File containing the smileobjectrelationlistHandler class.
  *
+ * @copyright Copyright (C) 1999 - 2014 Brookins Consulting. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or any later version)
+ * @version //autogentag//
+ * @package bccie
  */
+
+include_once( 'extension/bccie/classes/basehandler.php' );
+ 
 class smileobjectrelationlistHandler extends BaseHandler
 {
     function exportAttribute( &$attribute, $seperationChar )
