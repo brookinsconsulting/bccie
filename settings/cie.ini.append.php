a75b721be798a440d5f8ef16a9827c9c815217fc
Brookins Consulting <info@brookinsconsulting.com>
2015-05-23 03:45:39 -0500

    Updated: Added cie.ini setting DisplayLeaveEmptyOption to control display of export form option

--- settings/cie.ini.append.php
+++ settings/cie.ini.append.php
@@ -56,15 +56,24 @@ SylkSeparator=;
 # string instead of "0" collected value
 ExportZeroToEmptyString=disabled
 
+# Optional setting to disable display
+# of 'Leave empty (note: field still gets created)'
+# option in export module view
+DisplayLeaveEmptyOption=enabled
+
+# Required settings to partially control
+# export file name and date format
 ExportFileName=bccie_cie_export-
 ExportFileNameDateFormat=Y-m-d_H-i-s
 
+# Report / Export Format Control Handlers
 ExportOutputFormatHandlers[]
 ExportOutputFormatHandlers[utf8]=bccieExportFormatOutputHandlerUtf8
 ExportOutputFormatHandlers[utf8bom]=bccieExportFormatOutputHandlerUtf8Bom
 ExportOutputFormatHandlers[utf16le]=bccieExportFormatOutputHandlerUtf16Le
 ExportOutputFormatHandlers[cp1252]=bccieExportFormatOutputHandlerCP1252
 
+# Report / Export Format Control Handler Default
 ExportOutputFormatHandlerDefault=utf8
 
 */ ?>
