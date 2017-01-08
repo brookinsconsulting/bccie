ad1acdec42828ed1aee2527d2586a4e71994443f
Brookins Consulting <info@brookinsconsulting.com>
2017-01-08 11:58:54 -0600

    Updated: Expanded extension module view security regarding user role policy permissions comment explanation

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -7,8 +7,11 @@
 TranslationExtensions[]=bccie
 
 #
-# Uncomment the following to enable module views regardless of missing role policy configuration
-#
+# Security Concern: Uncomment the following to enable module views regardless of missing role policy configuration.
+# These settings were disabled by default in 2014 due to a confirmed report that they 'could' be used in a bccie
+# configuration with eZ Publish which results in anonymous collected information export from user siteaccesses.
+# These module view policies should be set instead within the eZ Publish Admin role policy configuration administration.
+# 
 # [RoleSettings]
 # PolicyOmitList[]=bccie/overview
 # PolicyOmitList[]=bccie/export
